af10409f16f0bdca3f06d4dece21f1e74aa12dd2
HuyVu <vuquanghuy2k@gmail.com>
2023-01-01 16:15:50 +0700

    Update W5 problems set

--- W5/main.py
+++ W5/main.py
@@ -38,10 +38,10 @@ class Task:
 
 class TaskRepository:
     def __init__(self) -> None:
-        pass
+        self.connection = sqlite3.connect('W5/todo.sqlite3') 
 
     def create_task(self, task: Task):
-        with sqlite3.connect('W5/todo.sqlite3') as conn:
+        with self.connection as conn:
             cur = conn.cursor()
             cur.execute(
                 '''
@@ -53,7 +53,7 @@ class TaskRepository:
             conn.commit()
 
     def create_many_tasks(self, tasks: list[Task]):
-        with sqlite3.connect('W5/todo.sqlite3') as conn:
+        with self.connection as conn:
             cur = conn.cursor()
             cur.executemany('''
                 INSERT INTO tasks (title, description, priority)
@@ -63,11 +63,31 @@ class TaskRepository:
             conn.commit()
 
     def get_all_tasks(self):
-        with sqlite3.connect('W5/todo.sqlite3') as conn:
+        with self.connection as conn:
             cur = conn.cursor()
             rows = cur.execute('SELECT * FROM tasks')
         
         return rows
+
+    def find_task_by_title(self, title: str):
+        with self.connection as conn:
+            cur = conn.cursor()
+            rows = cur.execute(f"SELECT * FROM tasks WHERE title = ?", (title, ))
+            rows.fetchone()
+
+        return row
+
+    def update_task_description(self, task_id: int, description: str) -> None:
+        with self.connection as conn:
+            cur = conn.cursor()
+            cur.execute('UPDATE tasks SET description = ? WHERE id = ?', (description, task_id))
+            conn.commit()
+
+    def delete_task(self, task_id: int) -> None:
+        with self.connection as conn:
+            cur = conn.cursor()
+            cur.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+            conn.commit()
 
 
 if __name__ == '__main__':
@@ -75,9 +95,21 @@ if __name__ == '__main__':
 
     repo = TaskRepository()
 
-    task = Task('Quét nhà', '', 1)
+    task = Task('Lau bàn', '', 1)
 
     repo.create_task(task)
 
     for row in repo.get_all_tasks():
         print(row)
+
+    # Find task
+    rec = repo.find_task_by_title('Lau bàn')
+
+    print()
+    print(rec)
+
+    # Udpate task
+    rec = repo.find_task_by_title('Lau bàn')
+    repo.update_task_description(rec[0], 'New description')
+
+    repo.delete_task(1)
