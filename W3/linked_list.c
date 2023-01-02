8853a53fa91241c79826ca6ae37e5909425c6095
HuyVu <vuquanghuy2k@gmail.com>
2023-01-02 15:14:55 +0700

    Update complete Linked list problem set

--- W3/linked_list.c
+++ W3/linked_list.c
@@ -20,41 +20,100 @@ Arrow opperator: student->age
 
 typedef struct node {
   int value;
-  int length;
   struct node *next;
 } node;
 
-void getAllNode(node *current) {
-  while (current != NULL) {
-    printf("%d\t%p\n", current->value, current->next);
+void getAllNode(node *head) {
+  while (head != NULL) {
+    printf("%p\t%d\t%p\n", head, head->value, head->next);
+    head = head->next;
+  }
+  printf("\n\n");
+}
+
+void pushNode(node *n, int value) {
+  // Add new node to the end
+  node *current = n;
+  while (current->next != NULL) {
     current = current->next;
   }
+  current->next = (node *)malloc(sizeof(node));
+  current->next->value = value;
+  current->next->next = NULL;
+}
+
+void popFirst(node **head) {
+  if ((*head)->next == NULL) {
+    free((*head));
+  }
+  node *oldHead = *head;
+  *head = (*head)->next;
+  free(oldHead);
+}
+
+void popLast(node *head) {
+  if (head->next == NULL) {
+    free(head);
+  }
+  node *current = head;
+  while (current->next->next != NULL) {
+    current = current->next;
+  }
+  free(current->next);
+  current->next = NULL;
+}
+
+void popNode(node **head, int n) {
+  // Pop node Nth
+  node *current = *head;
+  node *prevNode;
+  node *backNode;
+  if (n == 1) {
+    return popFirst(head);
+  }
+
+  for (int i = 1; i < n - 1; i++) {
+    if (current->next == NULL) {
+      printf("There is no next node at index %d\n", i);
+      return;
+    }
+    current = current->next;
+  }
+  prevNode = current;
+  backNode = current->next->next;
+
+  free(current->next);
+
+  prevNode->next = backNode;
+}
+
+node *getNode(node *head, int n) {
+  // Get Nth node
+  node *current = head;
+  for (int i = 1; i < n; i++) {
+    current = current->next;
+  }
+  return current;
 }
 
 int main(void) {
-  node *head;
-  head = (node *)malloc(sizeof(node));
+  node *head = (node *)malloc(sizeof(node));
   head->value = 1;
-  head->length = 1;
-  head->next = (node *)malloc(sizeof(node));
-
-  head->length += 1;
-
-  head->next->length = head->length;
-  head->next->value = 2;
-  head->next->next = NULL;
-
-  node *tail = (node *)malloc(sizeof(node));
-  tail->value = 3;
-  head->length += 1;
-
-  tail->length = head->length;
-  head->next->next = tail;
+  head->next = NULL;
+  pushNode(head, 2);
+  pushNode(head, 3);
+  pushNode(head, 4);
+  pushNode(head, 5);
 
   getAllNode(head);
 
-  free(head->next);
-  free(tail);
-  free(head);
+  popNode(&head, 1);
+
+  popLast(head);
+
+  getAllNode(head);
+
+  printf("3rd node address: %p\n", getNode(head, 3));
+
   return 0;
 }
