15b32768eadf23bcba20b3eae6665cd77a7e940c
HuyQuangVu <huy.vu@greenphire.com>
2023-02-18 12:00:58 +0700

    [W0] Add math_operator examples

---
+++ W0/math_operator.py
@@ -0,0 +1,15 @@
+a = 1
+b = 2
+
+# Perform math operator on numbers
+print(a + b) # Expected result: 3
+print(a - b) # Expected result: -1
+print(a * b) # Expected result: 2
+print(a / b) # Expected result: 0.5
+
+c = 'hello'
+d = 'world!'
+
+# Perform math operator on string
+print(c + d) # Expected result: helloworld!
+print(c * 3) # Expected result: hellohellohello
